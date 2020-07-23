9ded5a073ae16ff4271a3d575cdd3f78fd41d642
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-07-23 18:14:16 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,75 +1,70 @@
+function createCalendar() {
+  for (var i = 1; i <= daysInMonth; i++) {
+     var target = $("#calendar-list");
+     var data = "<li data-day='" + i + "' data-month='" + monthInNumber + "'>" + i + " " + month + " 2018</li>"
+
+    target.append(data);
+     };
+}
+
+function appendHolidays () {
+  $.ajax ({
+     url : 'https://flynn.boolean.careers/exercises/api/holidays',
+     method : 'GET',
+     data : {
+       'year': year,
+       'month': (monthInNumber - 1)
+            },
+     success : function(data, state) {
+       var success = data['success'];
+       var holidays = data['response'];
+       if (success) {
+         for (var i = 0; i < holidays.length; i++) {
+
+            var holiday = holidays[i];
+            var date  = holiday["date"];
+            var name = holiday["name"];
+            var date = moment(date);
+            date = date.format ("YYYY-M-D");
+
+
+            $( "li" ).each(function() {
+              var currentDay = $(this);
+              var day = currentDay.data("day");
+              var month = currentDay.data("month");
+              var currentDate = "2018-" + month + "-" + day;
+                if (currentDate == date) {
+                  $(this).append("<span>- " + name + "</span>");
+                  $(this).addClass("active");
+                }
+              })
+             }
+         }
+        else {
+         console.log(error);
+       }
+     },
+     error: function(request, state, error) {
+       console.log('request' , request);
+       console.log('state' , state);
+       console.log('error' , error);
+     }
+    });
+}
+
 
 function init() {
-var calendar = moment("2018-12-01", "YYYY-MM-DD");
-var month = calendar.format("MMMM");
-var monthInNumber = calendar.format("M");
-var year = calendar.format("YYYY");
 
-console.log(monthInNumber);
-var daysInMonth = moment().daysInMonth(year + "-" + month, "YYYY-MM");
-var target = $("#calendar-list");
-for (var i = 1; i <= daysInMonth; i++) {
-   var data = "<li data-day='" + i + "' data-month='" + monthInNumber + "'>" + i + " " + month + " 2018</li>"
-
-  target.append(data);
-   };
-
-  $.ajax ({
- url : 'https://flynn.boolean.careers/exercises/api/holidays',
- method : 'GET',
- data : {
-   'year': year,
-   'month': (monthInNumber - 1)
-
-  },
- success : function(data, state) {
-
-   var success = data['success'];
-   var holidays = data['response'];
-
-   if (success) {
-
-     console.log(holidays);
-
-      for (var i = 0; i < holidays.length; i++) {
-
-        var holiday = holidays[i];
-        var date  = holiday["date"];
-        var name = holiday["name"];
-        var date = moment(date);
-        date = date.format ("YYYY-M-D");
-        console.log(date);
-        // console.log(name);
-        // console.log(date);
-
-        $( "li" ).each(function() {
-          var currentDay = $(this);
-          var day = currentDay.data("day");
-          var month = currentDay.data("month");
-          var currentDate = "2018-" + month + "-" + day;
-          console.log(currentDate);
-          if (currentDate == date) {
-            $(this).append(" " + name);
-          }
-        })
+ calendar = moment("2018-04-01", "YYYY-MM-DD");
+ month = calendar.format("MMMM");
+ monthInNumber = calendar.format("M");
+ year = calendar.format("YYYY");
+ daysInMonth = moment().daysInMonth(year + "-" + month, "YYYY-MM");
 
 
+createCalendar();
 
-         }
-       }
-
-   else {
-     console.log(error);
-   }
- },
- error: function(request, state, error) {
-   console.log('request' , request);
-   console.log('state' , state);
-   console.log('error' , error);
- }
-
-
-});
+appendHolidays();
 
   };
 
